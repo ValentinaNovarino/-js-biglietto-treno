8aa8205236ea9cd3e8155a177410f917d3bc11fe
ValentinaNovarino <valenovarino@gmail.com>
2020-10-13 16:01:57 +0200

    calcolato sconto

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-// chiedere i chilometri da percorrere e l'età al passeggero e verificare che l'utente inserisca un numero
+// chiedere i chilometri da percorrere e l'età al passeggero e verificare che l'utente inserisca dei numeri
 
 var chilometri = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 
@@ -12,15 +12,28 @@ console.log(età);
 
 document.getElementById('età-passeggero').innerHTML = età;
 
-// calcolare il prezzo del biglietto in base alla distanza da percorrere. Considerando che il prezzo è di €0.21 / km
+// calcolare il prezzo generale del biglietto in base alla distanza da percorrere controllando che l'utente inserisca dei numeri. Il prezzo è di €0.21 / km
 
- if (!isNaN(chilometri)) {
+if (!isNaN(chilometri)) {
     console.log('è un numero');
-    var prezzoBigliettoGenerale = chilometri * 0.21;
+    var prezzoGenerale = chilometri * 0.21;
     console.log(chilometri * 0.21);
 } else {
     console.log('non è un numero');
 }
 
-
 // calcolare lo sconto in base all'età dell'utente. 20% per i minorenni 40% over 65
+
+if (età < 18) {
+    console.log('sconto 20% per minorenni');
+    var prezzoScontato = prezzoGenerale * 20 / 100;
+    console.log(prezzoGenerale * 20 / 100);
+} else if (età >65) {
+    console.log('sconto 40% per over 60');
+    var prezzoScontato = prezzoGenerale * 40 / 100;
+    console.log(prezzoGenerale * 40 / 100);
+} else {
+    console.log('prezzo biglietto generale');
+    var prezzoGenerale = chilometri * 0.21;
+    console.log(chilometri * 0.21);
+}
